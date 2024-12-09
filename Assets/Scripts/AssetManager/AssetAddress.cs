36e1146f2a18c70b76b597f49f58a8182b45396e
Igor Eliseev <ie.game.develop@gmail.com>
2024-12-09 14:53:15 +0400

    fix ui

--- Assets/Scripts/AssetManager/AssetAddress.cs
+++ Assets/Scripts/AssetManager/AssetAddress.cs
@@ -3,7 +3,7 @@ namespace AssetManager
     public static class AssetAddress
     {
         public const string PlayerPath = "Player/Player";
-        public const string HUDPath = "Player/HUD";
+        public const string HUDPath = "Player/PlayerUI";
         public const string CustomersPath = "Customers";
     }
 }
